1b802c7da19bf83b2870ab7be76fa673b7d209f6
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-07-14 00:24:55 +0530

    made an update api also

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-// Create Product
+// Create Product --- ADMIN
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -10,9 +10,30 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-
 // Get all Products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json({ success: true, products });
+};
+
+// Update Product --- ADMIN
+exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindandModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
 };
