e89561854a24a076f79c6b49d50b32ee1d1ea3bb
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-07-19 15:20:06 +0530

    added authRole and fetchUser

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,38 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
+const resError = require("../utils/resError");
 require("dotenv").config(); // Load environment variables from .env file, if present.
 
-const fetchUser = async (req, res, next) => {
+exports.fetchUser = async (req, res, next) => {
   const { authToken } = req.cookies;
 
   if (!authToken) {
-    return res.status(401).json({
-      success: false,
-      error: "Please authenticate using a valid token",
-    });
+    resError(401, "Please authenticate using a valid token", res);
   }
 
   try {
     const data = jwt.verify(authToken, process.env.JWT_SECRET);
 
-    req.user = await userModel.findById(data.id);
-
+    req.user = await userModel.findById(data.user.id);
 
     next();
   } catch (error) {
-    return res.status(401).json({
-      success: false,
-      error: "Please authenticate using valid token",
-    });
+    resError(401, "Please authenticate using valid token");
   }
 };
 
-module.exports = fetchUser;
+exports.authRole = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return resError(
+        403,
+        `Role: ${req.user.role} is not allowed to access this resource`,
+        res
+      );
+    }
+    next();
+  };
+};
+
+
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,14 +6,13 @@ const {
   deleteProduct,
   getProductDetails,
 } = require("../controllers/productController");
-const fetchUser = require("../middleware/auth");
-
+const {fetchUser, authRole} = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/products").get(fetchUser, getAllProducts);
-router.route("/product/new").post(createProduct);
-router.route("/product/update/:id").put(updateProduct);
-router.route("/product/delete/:id").delete(deleteProduct);
+router.route("/products").get(getAllProducts);
+router.route("/product/new").post(fetchUser, authRole("admin"), createProduct);
+router.route("/product/update/:id").put(fetchUser, authRole("admin"), updateProduct);
+router.route("/product/delete/:id").delete(fetchUser, authRole("admin"), deleteProduct);
 router.route("/product/get/:id").get(getProductDetails);
 
 module.exports = router;
