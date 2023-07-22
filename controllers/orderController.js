7ffaa1f8776cc459624932a899eb5d2d284d9f89
divy-03 <127951881+divy-03@users.noreply.github.com>
2023-07-22 19:08:13 +0530

    created some admin and user routes for order successfully

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -38,3 +38,90 @@ exports.newOrder = async (req, res) => {
     resError(500, `${error} => while creating order`, res);
   }
 };
+
+// Get Single Order --- ADMIN
+exports.getSingleOrder = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+
+    if (!order) {
+      return resError(404, "Order not found", res);
+    }
+
+    return res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    resError(500, `${error} => while getting order`, res);
+  }
+};
+
+// Get all my Order
+exports.myOrders = async (req, res) => {
+  try {
+    const orders = await Order.find({ user: req.user.id });
+
+    let totalAmount = 0;
+
+    orders.forEach((order) => {
+      totalAmount = totalAmount + order.totalPrice;
+    });
+
+    res.status(200).json({
+      success: true,
+      totalAmount,
+      orders,
+    });
+  } catch (error) {
+    resError(500, `${error} => while getting orders`, res);
+  }
+};
+
+// Get My Order
+exports.getMyOrder = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+    const userId = req.user.id;
+
+    // Find the order by both its ID and the current user ID
+    const order = await Order.findOne({ _id: orderId, user: userId })
+    if (!order) {
+      return resError(404, "Order not found", res);
+    }
+
+    return res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    resError(500, `${error} => while getting order`, res);
+  }
+};
+
+// Get All Orders --- ADMIN
+exports.getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find().populate(
+        "user",
+        "name email"
+      );
+
+    let totalAmount = 0;
+
+    orders.forEach((order) => {
+      totalAmount = totalAmount + order.totalPrice;
+    });
+
+    res.status(200).json({
+      success: true,
+      totalAmount,
+      orders,
+    });
+  } catch (error) {
+    resError(500, `${error}=>while getting orders`, res);
+  }
+};
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const { fetchUser, authRole } = require("../middleware/auth");
-const { newOrder } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getMyOrder,
+  getAllOrders,
+} = require("../controllers/orderController");
 
 router.route("/order/new").post(fetchUser, newOrder);
+router.route("/order/:id").get(fetchUser, authRole("admin", "owner"), getSingleOrder);
+router.route("/orders").get(fetchUser, myOrders);
+router.route("/my-order/:id").get(fetchUser, getMyOrder);
+router.route("/orders/all").get(fetchUser, authRole("admin", "owner"), getAllOrders);
 
 module.exports = router;
